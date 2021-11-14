e729ce2684b6a2c414c694f08600a097271d4482
Virgo-Alpha <bensonkingori96@gmail.com>
2021-11-14 14:35:53 +0400

    "anada"

--- console.py
+++ console.py
@@ -38,7 +38,8 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        """Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id"""
         if not (args):
             print("** class name missing **")
         elif args not in HBNBCommand.classes:
@@ -49,7 +50,8 @@ class HBNBCommand(cmd.Cmd):
             print(instance.id)
 
     def do_show(self, args):
-        """Prints the string representation of an instance based on the class name and id"""
+        """Prints the string representation of an instance
+        based on the class name and id"""
         if not (args):
             print("** class name missing **")
         else:
@@ -66,7 +68,8 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_destroy(self, args):
-        """Deletes an instance based on the class name and id (save the change into the JSON file)."""
+        """Deletes an instance based on the class name
+        and id (save the change into the JSON file)."""
         args = args.split()
         if not args:
             print("** class name missing **")
@@ -85,7 +88,8 @@ class HBNBCommand(cmd.Cmd):
         print("** no instance found **")
 
     def do_all(self, args):
-        """Prints all string representation of all instances based or not on the class name."""
+        """Prints all string representation of all instances
+        based or not on the class name."""
         split_args = shlex.split(args)
         n_list = []
         dict_json = models.storage.all()
@@ -103,7 +107,9 @@ class HBNBCommand(cmd.Cmd):
             print(n_list)
 
     def do_update(self, args):
-        """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)."""
+        """Updates an instance based on the class name and
+        id by adding or updating attribute
+        (save the change into the JSON file)."""
         args = args.split()
         if len(args) == 0:
             print("** class name missing **")
