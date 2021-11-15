32a695d2d91a52181d085eab28ca16033ba675a1
Virgo-Alpha <bensonkingori96@gmail.com>
2021-11-15 10:31:09 +0400

    "g"

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ FileStorage instance """
 
-from models.engine import FileStorage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.review import Review
