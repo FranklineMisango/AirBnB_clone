e5806f669c6365fea401ed8e76024d9c7dbf1659
Virgo-Alpha <bensonkingori96@gmail.com>
2021-11-23 20:18:51 +0400

    bjkkd

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-#from models.user import User
+from models.user import User
 from models.review import Review
 from models.place import Place
 from models.city import City
